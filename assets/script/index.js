96d2e7a6857aac020ed44c6982395a87b2c1784e
Seth Vandenbos <seth.vdbos@gmail.com>
2022-11-18 14:00:13 -0600

    working clock

--- assets/script/index.js
+++ assets/script/index.js
@@ -19,3 +19,24 @@ function selectAll(selector, parent = document) {
   return parent.querySelectorAll(selector);
 }
 
+const output = select('.current-time');
+const input = select('.input');
+const alarmTime = select('.alarm-time');
+const setAlarm = select('set-alarm');
+
+function currentTime() {
+  let current = new Date(); 
+  let hour = current.getHours();
+  let minute = current.getMinutes();
+  let second = current.getSeconds();
+
+  hour = hour.toString().padStart(2, '0');
+  minute = minute.toString().padStart(2, '0');
+  second = second.toString().padStart(2, '0');
+
+  let time = hour + ":" + minute + ":" + second;
+
+  output.innerText = time; 
+  let keepCurrent = setTimeout(function(){ currentTime() }, 1000);
+}
+currentTime();
